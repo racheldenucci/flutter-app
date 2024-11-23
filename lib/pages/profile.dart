fad309333b7bcbd2d9f4a826e1aaf6138bc4cefa
Rachel Denucci <denuccirachel@gmail.com>
2024-11-23 14:06:12 -0300

    barcode scanner added

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -10,41 +10,12 @@ class Profile extends StatefulWidget {
 
 class _ProfileState extends State<Profile> {
 
-  /* final List<Widget> pages = [
-    const Profile(),
-    const Scanner()
-  ];
-
-  int currentPage = 0; */
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Text('Este é o seu perfil'),
       ),
-      /* bottomNavigationBar: BottomNavigationBar(
-        currentIndex: currentPage,
-        onTap: (value){
-          setState(() {
-            currentPage = value;
-          });
-        },
-        items: const [
-          /* BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Início'
-          ), */
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Meu Perfil'
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.camera_alt_rounded),
-            label: 'Ler Código'
-          ),
-        ],
-      ), */
     );
   }
 }
--- lib/pages/scanner.dart
+++ lib/pages/scanner.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+//google books api key AIzaSyAdS3kr701E4g5sCCboerYPMIL6Hfas8qs
 
 class Scanner extends StatefulWidget {
   const Scanner({super.key});
@@ -8,13 +12,70 @@ class Scanner extends StatefulWidget {
 }
 
 class _ScannerState extends State<Scanner> {
+  String _scanBarcode = 'empty';
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<void> scanBarcodeNormal() async {
+    String barcodeScanRes;
+
+    try {
+      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+          '#ff6666', 'Cancel', true, ScanMode.BARCODE);
+      print(barcodeScanRes);
+    } on PlatformException {
+      barcodeScanRes = 'Failed to get platform version.';
+    }
+
+    if (!mounted) return;
+
+    setState(() {
+      _scanBarcode = barcodeScanRes;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body:
-        Center(
-          child: Text('Leitor de código de barras')
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Escanear código de barras')),
+        body: Builder(
+          builder: (BuildContext context) {
+            return Container(
+              alignment: Alignment.center,
+              child: Flex(
+                direction: Axis.vertical,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.orange[400],
+                      padding: EdgeInsets.all(14.0),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0)),
+                    ),
+                    onPressed: () => scanBarcodeNormal(),
+                    child: Text(
+                      'Escanear código de barras',
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold, fontSize: 16),
+                    ),
+                  ),
+                  SizedBox(height: 50),
+                  _scanBarcode != 'empty'
+                    ? Text('Código: $_scanBarcode\n',
+                      style: TextStyle(fontSize: 16)
+                    )
+                  : SizedBox(height: 0) 
+                ],
+              ),
+            );
+          },
         ),
+      ),
     );
   }
 }

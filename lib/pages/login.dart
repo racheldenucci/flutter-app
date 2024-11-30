7fae21f7cdca920bc445b40115ddf67b74dc28f7
Rachel Denucci <denuccirachel@gmail.com>
2024-11-30 14:46:38 -0300

    update

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -2,7 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_2/pages/main_page.dart';
 
-
 class Login extends StatefulWidget {
   const Login({super.key});
 
@@ -11,26 +10,27 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
-  
   //funcao de login
-  static Future<User?> login({required String email, required String password, required BuildContext context}) async{
+  static Future<User?> login(
+      {required String email,
+      required String password,
+      required BuildContext context}) async {
     FirebaseAuth auth = FirebaseAuth.instance;
     User? user;
-    try{
-      UserCredential userCredential = await auth.signInWithEmailAndPassword(email: email, password: password);
+    try {
+      UserCredential userCredential = await auth.signInWithEmailAndPassword(
+          email: email, password: password);
       user = userCredential.user;
-    } on FirebaseAuthException catch(e){
-      if(e.code == 'user-not-found'){
-        print('Este e-mail não está cadastrado');
-      }
+    } on FirebaseAuthException catch (e) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('Usuário ou senha inválidos!')));
     }
 
     return user;
   }
-  
+
   @override
   Widget build(BuildContext context) {
-
     TextEditingController _emailController = TextEditingController();
     TextEditingController _passwordController = TextEditingController();
 
@@ -91,17 +91,23 @@ class _LoginState extends State<Login> {
               padding: EdgeInsets.symmetric(vertical: 14.0),
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(8.0)),
-              onPressed: () async{
-                User? user = await login(email: _emailController.text, password: _passwordController.text, context: context);
+              onPressed: () async {
+                User? user = await login(
+                    email: _emailController.text,
+                    password: _passwordController.text,
+                    context: context);
                 print(user);
-                if(user != null){
-                  Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=> MainPage()));
+                if (user != null) {
+                  Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (context) => MainPage()));
                 }
               },
               child: Text(
                 'Entrar',
-                style:
-                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18.0),
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18.0),
               ),
             ),
             //TODO: ADD ERRO DE LOGIN
